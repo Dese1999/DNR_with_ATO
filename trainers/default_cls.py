01dd4593713445df8616d747ce81d62c9b4725e2
Dese1999 <mohadeseahmadii1378@gmail.com>
2025-06-02 18:21:23 +0330

    Update default_cls.py

--- trainers/default_cls.py
+++ trainers/default_cls.py
@@ -125,11 +125,11 @@ def soft_train(train_loader, model, hyper_net, criterion, valid_loader, optimize
             with torch.no_grad():
                 if cfg.project == 'gl':
                     if hasattr(model, 'module'):
-                         model.module.project_weight(hyper_net.transform_output(vector), lmdValue, model.lr)                        
+                         model.module.project_weight(hyper_net.vector2mask(vector), lmdValue, model.lr)                        
                     else:
-                        model.project_weight(hyper_net.transform_output(vector), lmdValue, model.lr)
+                        model.project_weight(hyper_net.vector2mask(vector), lmdValue, model.lr)
                 elif cfg.project == 'oto':
-                    model.oto(hyper_net.transform_output(vector))
+                    model.oto(hyper_net.vector2mask(vector))
 
         # Compute accuracy
         acc1, acc5 = accuracy(outputs, target, topk=(1, 5))
